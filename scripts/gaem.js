720f0154fe0ffc3d351e1856a9187463a511e38d
ermcookie <Mint_Cowokie@protonmail.com>
2025-08-06 22:42:35 +0100

    added blep

--- scripts/gaem.js
+++ scripts/gaem.js
@@ -3,3 +3,11 @@ function addPoints() {
     points = points + 1;
     document.getElementById("points").innerText = points;
 }
+function silly(x) {
+    if (x == 0) {
+        points = points + 1;
+        document.getElementById("points").innerText = points;
+        document.getElementById("blep").disabled = true;
+        document.getElementById("blep").innerText = "blepped :3";
+    }
+}
