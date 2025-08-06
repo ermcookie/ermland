3eab9bb62197974e434f3311734855d5717e5d06
ermcookie <Mint_Cowokie@protonmail.com>
2025-08-06 22:49:51 +0100

    added sillydance

--- scripts/gaem.js
+++ scripts/gaem.js
@@ -10,4 +10,14 @@ function silly(x) {
         document.getElementById("blep").disabled = true;
         document.getElementById("blep").innerText = "blepped :3";
     }
+    if (x == 1) {
+    if (points >= 5) {
+        points = points - 5;
+        document.getElementById("points").innerText = points;
+        document.getElementById("sillydance").disabled = true;
+        document.getElementById("sillydance").innerText = "sillydanced";
+    } else {
+        alert("you need at least 5 points to do the sillydance");
+    }
+}
 }
